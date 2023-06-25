2d57d0c70f07dbd11a8f357b45f97bca2b9c9be5
김승빈 <sbk930123@naver.com>
2023-06-26 02:05:05 +0900

    🐛

--- src/assets/infoData.js
+++ src/assets/infoData.js
@@ -4,27 +4,27 @@ export default [
     title: 'SI 사업',
     subTitle: 'SSA(Ssof Service Architecture)',
     infoText: '동일한 구성과 구조를 통해 개발되는 SW는 공동모듈을 통해 개발기간을 단축하고 다양한 솔루션을 단기간에 개발을 할 수 있습니다. SSA는 SSA기반으로 개발된 Package 솔루션, SI프로젝트를 통해 많은 고객이 경험 하였으며, 그 경험은 다시 SSA에 Know-how로 남아 있습니다.',
-    imgSrc: 'http://www.sisnetinfo.co.kr/wp-content/uploads/2019/01/fms_new.png'
+    imgSrc: '../assets/images/fms_new.png'
   },
   {
     id: 1,
     title: '해외 Biz',
     subTitle: 'POS 및 물류시스템',
     infoText: '국내에서 이룬 유통 시장의 성과와 경험을 바탕으로 해외 신규 사업을 개척하고 있습니다. 2019년 베트남 현지 88개 한인점포인 K Mart에 POS 및 물류시스템을 개발 공급하였고, 베트남 현지 리조트 통합 Solution을 개발 중에 있으며, 르완다 조세 당국에 전자영수증 고도화 사업을 진행중에 있습니다.',
-    imgSrc: 'http://www.sisnetinfo.co.kr/wp-content/uploads/2017/11/mainimg-server.png'
+    imgSrc: '../assets/images/mainimg-server.png'
   },
   {
     id: 2,
     title: 'IT 서비스 사업',
     subTitle: 'SLA(Service Level Agreement)',
     infoText: '쏘프는 전국 8,000여개의 나들가게 점포에 시스템 운영과 콜센터를 운영하고 있습니다. 쏘프가 개발한 Solution에 대한 국내 전 고객에게 서비스가 가능하고, SLA(Service Level Agreement)를 통해 서비스 품질을 검증 받으며, OCS(One Call Service)를 제공합니다.',
-    imgSrc: 'http://www.sisnetinfo.co.kr/wp-content/uploads/2017/11/main_maintanance.png'
+    imgSrc: '../assets/images/main_maintanance.png'
   },
   {
     id: 3,
     title: '(주)쏘프',
     subTitle: 'Smart Software',
     infoText: '스마트한 솔루션, 높은 퀄리티, 사용자 중심의 솔루션 제공을 위해 최선을 다하고 있으며, 넘치는 아이디어와, 무한한 열정으로 솔루션 개발에 앞장서고 싶다면, 지금 바로 쏘프에서 시작해보세요.',
-    imgSrc: 'http://www.sisnetinfo.co.kr/wp-content/uploads/2017/11/main_degausser.png'
+    imgSrc: '../assets/images/main_degausser.png'
   },
 ]
